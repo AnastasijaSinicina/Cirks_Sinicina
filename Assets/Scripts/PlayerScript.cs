47802e4631761b03b31d6eea99838f2116d4fdf5
Anastasija Sinicina <sinica.nastja@gmail.com>
2025-02-06 11:50:56 +0200

    characters

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.IO;
 
 public class PlayerScript : MonoBehaviour
 {
@@ -23,7 +24,31 @@ public class PlayerScript : MonoBehaviour
         GameObject mainCharacter = Instantiate(playerPrefabs[characterIndex], spawnPoint.transform.position, Quaternion.identity);
         mainCharacter.GetComponent<NameScript>().SetPlayerName(PlayerPrefs.GetString("PlayerName"));
 
-        //Turpināsim
+        otherPlayers = new int[PlayerPrefs.GetInt("PlayerCount")];
+        string[] nameArray = ReadLinesFromFile(textFileName);
+
+        for(int i=0; i<otherPlayers.Length-1; i++)
+        {
+            spawnPoint.transform.position += new Vector3(0.2f, 0, 0.08f);
+            index = Random.Range(0, playerPrefabs.Length - 1);
+            GameObject character = Instantiate(playerPrefabs[index],spawnPoint.transform.position, Quaternion.identity);
+            character.GetComponent<NameScript>().SetPlayerName(nameArray[Random.Range(0, nameArray.Length - 1)]);
+        }
+
+    }
+
+    string[] ReadLinesFromFile(string fName)
+    {
+        TextAsset textAsset = Resources.Load<TextAsset>(fName);
+        if (textAsset != null)
+        {
+            return textAsset.text.Split(new[] { '\r', '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+        }
+        else 
+        {
+            Debug.LogError("File not found: " + fName);
+            return new string[0];
+        }
     }
 
     // Update is called once per frame
